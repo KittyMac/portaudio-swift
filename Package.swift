d33b83e97356f6f51c1cc71fe4193e92dd47be9a
Rocco Bowling <rocco@chimerasw.com>
2020-07-09 16:49:33 -0400

    limit linking to asound to linux

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
 				.define("BUILD_ALSA", supportsALSA)
 		    ],
 			linkerSettings: [
-				.linkedLibrary("asound")
+				.linkedLibrary("asound", supportsALSA)
 			]),
         .target(
             name: "portaudio",
